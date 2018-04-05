6d27c9c79367816611c6368af00d918c57bd2ba3
BAnd313 <andrea.benini96@gmail.com>
2018-04-05 21:56:34 +0000

    src files

---
+++ src/controller.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+# license removed for brevity
+import rospy, signal, sys, readchar
+from std_msgs.msg import String
+
+
+def sigint_callback():
+    sys.exit(130)
+
+
+def controller():
+	pub = rospy.Publisher('controller', String, queue_size=10)
+	rospy.init_node('controller', anonymous=True)
+	rate = rospy.Rate(1) # 1hz
+	
+	#choice = raw_input("Digit a command: ")
+	choice = readchar.readkey()
+	
+	# SIGINT handler
+	if choice == '\x03':
+	    sigint_callback()
+	
+	if len(choice) == 1 and choice in "enac":
+	    rospy.loginfo(choice)
+	    pub.publish(choice)
+	
+	rate.sleep()
+
+if __name__ == '__main__':
+    
+    while True:
+	    controller()
--- src/talker.py
+++ src/talker.py
@@ -3,16 +3,23 @@
 import rospy
 from homework1.msg import Template
 
+message = Template()
+
+
+def setMessage() :
+	message.name = "Andrea"
+	message.age = 100
+	message.major = "Bachelor's degree in Computer Science "
+
+
 def talker():
 	pub = rospy.Publisher('/chatter', Template, queue_size=10)
 	rospy.init_node('talker', anonymous=True)
-	rate = rospy.Rate(1) # 10hz
-	message = Template()
+	rate = rospy.Rate(1) # 1hz
+
 	while not rospy.is_shutdown():
-		message.name = "Andrea"
-		message.age = 100
-		message.major = "Bachelor's degree in Computer Science "
-		#rospy.loginfo(hello_str)
+		setMessage()
+		rospy.loginfo(message)
 		pub.publish(message)
 		rate.sleep()
 
